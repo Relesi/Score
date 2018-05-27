69649d4152351ce364f547a75c7d515d15c9a7e9
Renato Lessa <renatolessa.2011@hotmail.com>
2018-05-27 19:00:05 -0300

    Criando instanciacao

--- src/main/java/com/relesi/score/ScoreApplication.java
+++ src/main/java/com/relesi/score/ScoreApplication.java
@@ -1,14 +1,34 @@
 package com.relesi.score;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.relesi.score.domain.Categoria;
+import com.relesi.score.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ScoreApplication {
+public class ScoreApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+
 	}
 }
