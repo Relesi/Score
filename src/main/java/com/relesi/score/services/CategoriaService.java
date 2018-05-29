5f7f13357b761c9a5854009169b916a91c6167a0
Renato Lessa <renatolessa.2011@hotmail.com>
2018-05-29 11:37:09 -0300

    Ajustes no endPoint / categorias{id}

--- src/main/java/com/relesi/score/services/CategoriaService.java
+++ src/main/java/com/relesi/score/services/CategoriaService.java
@@ -5,16 +5,22 @@ import org.springframework.stereotype.Service;
 
 import com.relesi.score.domain.Categoria;
 import com.relesi.score.repositories.CategoriaRepository;
+import com.relesi.score.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo " + Categoria.class.getName());
+		}
+
 		return obj;
 	}
-	
+
 }
