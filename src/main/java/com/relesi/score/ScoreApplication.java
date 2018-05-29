31050065d27c1e7bce280f85eb298ca4e293574c
Renato Lessa <renatolessa.2011@hotmail.com>
2018-05-29 11:03:48 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/relesi/score/ScoreApplication.java
+++ src/main/java/com/relesi/score/ScoreApplication.java
@@ -1,7 +1,5 @@
 package com.relesi.score;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.relesi.score.domain.Categoria;
+import com.relesi.score.domain.Produto;
 import com.relesi.score.repositories.CategoriaRepository;
+import com.relesi.score.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ScoreApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScoreApplication.class, args);
@@ -28,7 +31,19 @@ public class ScoreApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p1));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		produtoRepository.save(Arrays.asList(p1, p2, p3));
 
 	}
 }
