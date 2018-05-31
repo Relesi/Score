7b5573d1ed64b029fc194896b6c9d8bcd0fa0e01
Renato Lessa <renatolessa.2011@hotmail.com>
2018-05-31 15:47:36 -0300

    Estados e Cidades

--- src/main/java/com/relesi/score/ScoreApplication.java
+++ src/main/java/com/relesi/score/ScoreApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.relesi.score.domain.Categoria;
+import com.relesi.score.domain.Cidade;
+import com.relesi.score.domain.Estado;
 import com.relesi.score.domain.Produto;
 import com.relesi.score.repositories.CategoriaRepository;
+import com.relesi.score.repositories.CidadeRepository;
+import com.relesi.score.repositories.EstadoRepository;
 import com.relesi.score.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class ScoreApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScoreApplication.class, args);
@@ -41,9 +51,22 @@ public class ScoreApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-
+	
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.save(Arrays.asList(est1, est2));
+		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 
 	}
 }
