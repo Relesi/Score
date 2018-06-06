a3a5dbfce09bea13837c62812c77c21d27f78789
Renato Lessa <renatolessa.2011@hotmail.com>
2018-06-06 00:11:54 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/relesi/score/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.relesi.score.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.relesi.score.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
