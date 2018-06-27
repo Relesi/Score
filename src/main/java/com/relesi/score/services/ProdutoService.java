4925b017073e3f8e2440d6aea6694f55109f4c4f
Renato Lessa <renatolessa.2011@hotmail.com>
2018-06-27 10:48:56 -0300

    Busca por produtos por nome e categorias

---
+++ src/main/java/com/relesi/score/services/ProdutoService.java
@@ -0,0 +1,42 @@
+package com.relesi.score.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.relesi.score.domain.Categoria;
+import com.relesi.score.domain.Produto;
+import com.relesi.score.repositories.CategoriaRepository;
+import com.relesi.score.repositories.ProdutoRepository;
+import com.relesi.score.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Produto find(Integer id) {
+		Produto obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo " + Produto.class.getName());
+		}
+
+		return obj;
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAll(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+
+}
