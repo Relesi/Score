c572c18c278b91650611acd6c5e2f3dcbd5de77f
Renato Lessa <renatolessa.2011@hotmail.com>
2018-06-27 11:03:03 -0300

    Subtotal e total do Pedido

--- src/main/java/com/relesi/score/domain/ItemPedido.java
+++ src/main/java/com/relesi/score/domain/ItemPedido.java
@@ -33,6 +33,10 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 	
+	public double getSubTotal() {
+		return (preco - desconto) * quantidades;
+	}
+	
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
