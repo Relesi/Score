45e12e4f15aaef5f57b531d02d8090926b7c8f34
Renato Lessa <renatolessa.2011@hotmail.com>
2018-07-09 21:33:48 -0300

    Email HTML

--- src/main/java/com/relesi/score/services/EmailService.java
+++ src/main/java/com/relesi/score/services/EmailService.java
@@ -1,12 +1,20 @@
 package com.relesi.score.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.relesi.score.domain.Pedido;
+
+
 
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
