c90f5bfe5eda719d6dc551bcc87bf23e6b04c19f
Renato Lessa <renatolessa.2011@hotmail.com>
2018-07-25 20:25:44 -0300

    Adicionando senha a Cliente

--- src/main/java/com/relesi/score/services/DBService.java
+++ src/main/java/com/relesi/score/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.relesi.score.domain.Categoria;
@@ -33,6 +34,9 @@ import com.relesi.score.repositories.ProdutoRepository;
 
 @Service
 public class DBService {
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 
 	
 	@Autowired
@@ -126,7 +130,7 @@ public class DBService {
 		estadoRepository.save(Arrays.asList(est1, est2));
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "hendix.page@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "hendix.page@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
