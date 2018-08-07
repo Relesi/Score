17d022399632bf4dbd96cf78203996a58d9a645a
Renato Lessa <renatolessa.2011@hotmail.com>
2018-08-06 21:53:26 -0300

    Implementando autentication e geracao do token JWT

---
+++ src/main/java/com/relesi/score/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.relesi.score.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.relesi.score.domain.Cliente;
+import com.relesi.score.repositories.ClienteRepository;
+import com.relesi.score.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
