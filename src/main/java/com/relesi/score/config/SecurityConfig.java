c1800802c04b7bf8a3b806f1bf196765ed7b227d
Renato Lessa <renatolessa.2011@hotmail.com>
2018-08-15 22:07:41 -0300

    Esqueci a senha

--- src/main/java/com/relesi/score/config/SecurityConfig.java
+++ src/main/java/com/relesi/score/config/SecurityConfig.java
@@ -45,12 +45,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
-			"/categorias/**",
+			"/categorias/**"
 			
 	};
 
 	private static final String[] PUBLIC_MATCHERS_POST = {
-			"/clientes/**"
+			"/clientes/**",
+			"/auth/forgot/**"
 			
 			
 	};
--- src/main/java/com/relesi/score/services/EmailService.java
+++ src/main/java/com/relesi/score/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.relesi.score.domain.Cliente;
 import com.relesi.score.domain.Pedido;
 
 
@@ -17,4 +18,6 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendHtmlEmail(MimeMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
