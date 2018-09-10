ff59f8236f8b793214b23684ff9ce3e282454f02
Renato Lessa <renatolessa.2011@hotmail.com>
2018-09-10 20:51:21 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/relesi/score/ScoreApplication.java
+++ src/main/java/com/relesi/score/ScoreApplication.java
@@ -1,12 +1,18 @@
 package com.relesi.score;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.relesi.score.services.S3Service;
+
 @SpringBootApplication
 public class ScoreApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ScoreApplication.class, args);
 
@@ -15,5 +21,7 @@ public class ScoreApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		s3Service.uploadFile("C:\\temp\\fotos\\escritorio1.jpg");
+		
 	}
 }
