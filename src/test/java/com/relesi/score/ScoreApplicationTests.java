a192416d1c8c64efd93f7a64507eb27108725e61
Renato Lessa <renatolessa.2011@hotmail.com>
2018-10-20 20:08:53 -0300

    Expondo o header Authorization

--- src/test/java/com/relesi/score/ScoreApplicationTests.java
+++ src/test/java/com/relesi/score/ScoreApplicationTests.java
@@ -1,18 +1,18 @@
 package com.relesi.score;
 
 
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.TestPropertySource;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
+@SpringBootTest(classes = ScoreApplication.class)
+@TestPropertySource(locations="classpath:test.properties")
 public class ScoreApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
+	
 
 }
